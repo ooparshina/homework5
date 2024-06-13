eac70a63f91604733f45be87f6e5409042362052
ooparshina <ooparshina@gmail.com>
2024-06-13 14:34:15 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = "дом", 15, 5.84, True, [13, 28, 56]
+print(immutable_var)
+mutable_list = [4, 15, 'собака', 'кошка', False]
+print(mutable_list)
+mutable_list[1] = 48
+mutable_list[3] = True
+mutable_list[4] = "пицца"
+print(mutable_list)
