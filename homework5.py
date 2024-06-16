ed2f445c6ee13f2f66b6794d190be2f3391229b4
ooparshina <ooparshina@gmail.com>
2024-06-16 17:58:35 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,5 +1,8 @@
 immutable_var = "дом", 15, 5.84, True, [13, 28, 56]
 print(immutable_var)
+#immutable_var[0] = "крыша"
+#print(immutable_var)
+# TypeError: 'tuple' object does not support item assignment
 mutable_list = [4, 15, 'собака', 'кошка', False]
 print(mutable_list)
 mutable_list[1] = 48
